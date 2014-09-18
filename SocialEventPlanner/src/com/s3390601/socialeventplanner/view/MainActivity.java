bea0a1d1ec2f64b93725c30681f0126a90c9d119
s3390601 <s3390601@student.rmit.edu.au>
2014-09-18 17:49:01 +1000

    Commit before starting Assignment2

--- SocialEventPlanner/src/com/s3390601/socialeventplanner/view/MainActivity.java
+++ SocialEventPlanner/src/com/s3390601/socialeventplanner/view/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends ListActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list_view);
-        testMethod();
+        //testMethod();
         getActionBar().setDisplayHomeAsUpEnabled(false);
   
         
